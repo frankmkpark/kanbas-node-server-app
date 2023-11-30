589212977ada98341faf17f883291a704edeafee
frankmkpark <frankmkpark@gmail.com>
2023-11-30 18:19:59 -0500

    Upto 3.5.4 completed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from './modules/routes.js';
 import cors from "cors";
 import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import "dotenv/config";
 const app = express()
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app);
 Hello(app)
 Lab5(app);
 app.listen(process.env.PORT || 4000);
