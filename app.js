6114a35cfd933bb39b669dd2945600ddb5ad7627
frankmkpark <frankmkpark@gmail.com>
2023-12-03 00:18:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import express from 'express';
 import session from "express-session";
 import cors from "cors";
 import mongoose from "mongoose";
-import connectMongo from "connect-mongo"; // For production-ready session store
+import connectMongo from "connect-mongo";
 
 // Importing route modules
 import Lab5 from "./lab5.js";
@@ -14,18 +14,15 @@ import ModuleRoutes from './modules/routes.js';
 import UserRoutes from "./users/routes.js";
 
 // MongoDB connection setup
-const MongoStore = connectMongo.create({ mongoUrl: CONNECTION_STRING });
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
-
-// Fail fast if DB_CONNECTION_STRING is not set
 if (!CONNECTION_STRING) {
   console.error('MongoDB connection string is not set.');
   process.exit(1);
 }
+mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
+// Connect-mongo session store setup
+const MongoStore = connectMongo.create({ mongoUrl: CONNECTION_STRING });
 
 // Express application setup
 const app = express();
@@ -33,7 +30,7 @@ const app = express();
 // CORS configuration
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL, // Your frontend application's URL
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -43,16 +40,13 @@ app.use(express.json());
 
 // Session configuration for production
 const sessionOptions = {
-  secret: "any string",
+  secret: "your-session-secret",
   resave: false,
   saveUninitialized: false,
-  store: new MongoStore({ 
-    mongooseConnection: mongoose.connection,
-    collection: 'sessions' // Optional: define sessions collection name
-  }),
+  store: new MongoStore,
   cookie: {
-    secure: process.env.NODE_ENV === "production", // Cookies are only over HTTPS
-    httpOnly: true // Prevents client side JS from reading the cookie 
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true 
   }
 };
 
